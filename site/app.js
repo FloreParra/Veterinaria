622b0c8882d60a3fd9df2f22f422e4a4178696ac
Florencia Parra <fnparra@gmail.com>
2021-09-17 16:42:11 -0300

    post productAdd

--- site/app.js
+++ site/app.js
@@ -11,6 +11,10 @@ app.set("view engine", "ejs");
 // Puerto
 app.listen(3001, () => console.log("puerto funcionando 3001"));
 
+// Archivos por POST
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
 // Archivos estáticos
 app.use(express.static(path.resolve(__dirname, "./src/public")));
 
--- site/controller/host.js
+++ site/controller/host.js
@@ -5,6 +5,16 @@ const hostControler = {
   addProduct: (req, res) => {
     res.render("host/addProduct");
   },
+  createProduct: (req, res) => {
+    let datos = {
+      name: req.params.name,
+      picture: req.params.picture,
+      price: req.params.price,
+      description: req.params.description,
+    };
+    console.log(datos);
+    res.redirect("main/products");
+  },
   hc: (req, res) => {
     res.render("host/hc");
   },
